a457d30e609ac85fb6c9b72e5ed639393865203d
VicMCA (Victor) <victor.mcavalcanti@gmail.com>
2021-09-08 00:26:36 -0300

    Adicionado exercicio do cronometro

---
+++ javascriptStudy/ExerciseOOP01_MakeAStopwatch.js
@@ -0,0 +1,87 @@
+function main() {
+    /* Let's create a stopwatch!
+    Include the properties:
+    sw.duration
+    and the methods:
+    sw.reset() to reset the watch
+    sw.start() to start counting the time
+    sw.stop() to stop the count
+    The methods must detect if they've already been run or not
+    The class will also have
+    constructor, hasOwnProperty, isPropertyOf, propertyIsEnumerable,
+    toLocaleString, toString, valueOf
+     */
+    class Stopwatch {
+        constructor() {
+            let startTime, endTime;
+            let isRunning;
+            let runTime = 0;
+
+            this.start = function() {
+                if (isRunning)
+                    throw new Error('The stopwatch is already running');
+                isRunning = true;
+                startTime = new Date();
+            }
+
+            this.stop = function() {
+                if (!isRunning)
+                    throw new Error('The stopwatch is already stopped');
+                endTime = new Date();
+                isRunning = false;
+                
+                const seconds = (endTime.getTime() - startTime.getTime()) /1000;
+                runTime += seconds;
+            }
+
+            this.reset = function() {
+                startTime = null;
+                endTime = null;
+                isRunning = false;
+                runTime = 0;
+            }
+
+            Object.defineProperty(this, 'runTime', {
+                get: function() { return runTime; }
+            })
+        }
+    }
+
+    const sw = new Stopwatch();
+    
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    let swUse = function() {
+        readline.question('Stopwatch controls = start, stop, reset, show, quit \n>> ', command => {
+            if (command === 'start') {
+                sw.start();
+                swUse();
+            } else if (command === 'stop') {
+                sw.stop();
+                swUse();
+            } else if (command === 'reset') {
+                sw.reset();
+                swUse();
+            } else if (command === 'show') {
+                console.log(sw.runTime);
+                swUse();
+            } else if (command === 'quit') {
+                readline.close();
+                process.stdin.destroy();
+            } else {
+                console.log('Invalid input.')
+                swUse();
+            }
+        });
+    }
+
+    swUse();
+    
+}
+
+if (require.main === module) {
+    main();
+}
