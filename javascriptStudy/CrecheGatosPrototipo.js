e1203cf831c68208c3d41cd8eb7d04304a3456df
VicMCA (Victor) <victor.mcavalcanti@gmail.com>
2021-09-14 19:15:34 -0300

    Progress on studies and exercises

--- javascriptStudy/CrecheGatosPrototipo.js
+++ javascriptStudy/CrecheGatosPrototipo.js
@@ -1,3 +1,5 @@
+const { ExitStatus } = require('typescript');
+
 class Gato {
     constructor(name, age) { // It'll receive 2 parameters
         this.name = name;
@@ -33,9 +35,45 @@ function main() {
     " `-´~~~ (,__....----'''       (,....-'\n"+
     "🐱 Olá, bem-vindo(a) ao Gatotel! 🐱");
 
-    gato = criarCadastro();
+    menuInicial();
     console.log("Lista de gatos cadastrados: " + gatoLista);
     teste();    
+}
+
+
+function menuInicial() {
+    const read = require('readline-sync');    
+    
+    console.log('🧶🐈 O que deseja fazer? Digite o número da opção escolhida:');
+    resposta = read.question('[1] Cadastrar bichano\n[2] Consultar bichano\n[3] Administrar bichano\n[4] Sair\n>> ');
+
+    switch (resposta) {
+        case '1':
+            gato = criarCadastro();
+            /*
+            Cada cadastro criado está sobrescrevendo o objeto ao invés de criar um novo :(
+            Para progredir terei de salvar os objetos em um banco de dados .json,
+            e a cada vez que for interagir com um objeto devo usar o banco de dados
+            para recriar o objeto com os atributos com os quais foi salvo, e ao encerrar
+            a interação devo salvá-lo de novo com as modificações
+            */
+            break;
+        case '2':
+            teste();
+            break;
+        case '3':
+            // escolhaGato(gatoLista);
+            break;
+        case '4':
+            encerrarPrograma();
+            break;
+        default:
+            console.log('Dado inválido. Informe novamente');
+            menuInicial();
+            break;
+    }
+
+    menuInicial();
 }
 
 
@@ -60,10 +98,21 @@ function cadastro(infoPacote) {
 
 
 function teste() {
-    eval(`${gatoLista[1]}.status();`);
+    const read = require('readline-sync');
+    console.log("Lista de gatos cadastrados: " + gatoLista);
+
+    let nome = read.question('Qual bichano deseja consultar?\n>> ');
+    eval(`${gatoLista[nome-1]}.status();`);
+}
+
+
+function encerrarPrograma() {
+    Exit
+    return 1;
 }
 
 
 if (require.main === module) {
     main();
 }
+
